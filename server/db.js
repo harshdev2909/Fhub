a89bda8a0968d6df6cc0e60368ec3ece9e658562
harshdev2909 <harsh.dev2909@gmail.com>
2024-08-24 17:46:56 +0530

    Database Crashed

--- server/db.js
+++ server/db.js
@@ -1,10 +1,19 @@
-const mongoos = require('mongoose');
-const mongoURI = 'mongodb+srv://harshsharmaa990:harsh9560@cluster0.cb5f1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://harshsharmaa990:harsh9560@cluster0.cb5f1.mongodb.net/harsh?retryWrites=true&w=majority&appName=Cluster0';
 
-const mongoDb = ()=>{
- mongoos.connect(mongoURI)
+const mongoDb = async ()=>{
+   await mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+  const fetched_data = mongoose.connection.db.collection("Food_items");
+  fetched_data.find({}).toArray(function(err,data){
+    if(err) console.log(err);
+    else console.log(data)
+  })
 }
-if(mongoDb){
-    console.log("database Connected")
-}
+
+
 module.exports = mongoDb;
+
+
+
