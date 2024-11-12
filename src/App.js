9815ab95840c815088f9754f0d6c20399ac287ed
harshdev2909 <harsh.dev2909@gmail.com>
2024-11-12 20:17:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import { Login } from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import CartContextProvider from './context/CartContextProvider.js';
-import CartContext from './context/CartContext.js';
+
 function App() {
   return (
     <>
